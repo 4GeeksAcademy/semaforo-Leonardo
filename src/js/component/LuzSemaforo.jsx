37d7d8aba690e73a59161927ece0288ea010f722
Leonardo Medina <leonardomedina1250@gmail.com>
2024-10-30 10:00:30 +0000

    commit 30-10

--- src/js/component/LuzSemaforo.jsx
+++ src/js/component/LuzSemaforo.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 
-const LuzSemaforo = (props) => {
+const LuzSemaforo = ({ color, brillo, onClick, isActive }) => {
+    const brilloClass = brillo == "encendido" ? "brillo" : null;
+    const activeClass = isActive ? "active" : "";
+
     return (
-        <button id={props.brillo} className={props.color}></button>
+        <button 
+            id={brillo} 
+            className={`${color} ${brilloClass} ${activeClass} mt-4 mb-4`} 
+            
+            onClick={onClick}
+        >
+        </button>
     );
 };
+
 export default LuzSemaforo;
